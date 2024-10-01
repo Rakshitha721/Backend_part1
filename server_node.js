8fa48c0431a882aa557f0767e7db3738a7eac729
rakshitha <rakshithashekar72@gmail.com>
2024-10-01 07:59:37 +0530

    Added server-bun.js and server_node.js

---
+++ server_node.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if(req.url =='/'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello Rakshitha");}
+    else if(req.url =='/rakshitha'){
+        res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("do it for yourself rakshitha");
+
+    }
+    else{
+        res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("404 not found");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is listening at http://${hostname}:${port}`);
+});
